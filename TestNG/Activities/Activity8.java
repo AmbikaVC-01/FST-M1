911053c957a959da28d63c1b928a3ee4601f4bb8
AmbikaVC-01 <ambika.ambika1@ibm.com>
2024-09-20 17:52:32 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity8.java
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd" >
+<suite name="TestSuite" parallel="classes">
+    <test name="Tests">
+        <classes>
+            <class name="com.ibm.fst_m1_testng.Activity1"/>
+            <class name="com.ibm.fst_m1_testng.Activity2"/>
+            <class name="com.ibm.fst_m1_testng.Activity3"/>
+	    <class name="com.ibm.fst_m1_testng.Activity4D1"/>
+            <class name="com.ibm.fst_m1_testng.Activity4D2"/>
+            <class name="com.ibm.fst_m1_testng.Activity5"/>
+            <class name="com.ibm.fst_m1_testng.Activity6"/>
+        </classes>
+    </test>
+</suite>
